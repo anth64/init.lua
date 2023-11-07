8fe42c6e06dd88d4558d28737cdef50b7e566c2d
anth64 <anth64@protonmail.com>
2023-11-07 16:16:20 -0500

    Initial configuration.

---
+++ after/plugin/colors.lua
@@ -0,0 +1,16 @@
+require('tokyonight').setup({
+  style = "night",
+  styles = {
+    comments = { italic = false },
+    keywords = { italic = false }
+  }
+})
+
+function ColorMyPencils(color)
+  color = color or 'tokyonight'
+	vim.cmd.colorscheme(color)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils()
---
+++ lua/anth64/init.lua
@@ -0,0 +1,2 @@
+require("anth64.set")
+require("anth64.remap")
