7b3f194aac767973538651b012c1dbe84c88fd27
anth64 <anth64@protonmail.com>
2024-11-18 15:28:40 +0100

    spacing

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,5 @@
 local config = {
-  cmd = { vim.fn.expand("~/.local/share/jdtls/bin/jdtls") },
-  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+    cmd = {'/home/anth/.local/share/java/jdtls/bin/jdtls'},
+    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
