bc612976f799c1235373bdd847dcd37498ca6ee4
anth64 <anth64@protonmail.com>
2024-11-18 15:27:39 +0100

    use neovim default if color is not found.

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -11,10 +11,10 @@ require('night-owl').setup({
 })
 
 function ColorMyPencils(color)
-  color = color or 'tokyonight'
+  color = color or 'default'
 	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils('night-owl')
