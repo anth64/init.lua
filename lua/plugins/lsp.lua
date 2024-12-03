eb50cc0a3495e4f5b039ac11e0fbe6cc50b4c25b
anth64 <anth64@protonmail.com>
2024-12-03 21:57:52 +0100

    fixed issue where auto-complete was borked...

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,18 +16,20 @@ return {
       "hrsh7th/cmp-nvim-lsp"
     },
     config = function()
-      require("lsp-zero")
       vim.opt.signcolumn = 'yes'
+
       local lspconfig_defaults = require('lspconfig').util.default_config
       lspconfig_defaults.capabilities = vim.tbl_deep_extend(
         'force',
         lspconfig_defaults.capabilities,
         require('cmp_nvim_lsp').default_capabilities()
       )
+
       vim.api.nvim_create_autocmd('LspAttach', {
         desc = 'LSP actions',
         callback = function(event)
           local opts = { buffer = event.buf }
+
           vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
           vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
           vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
@@ -39,6 +41,23 @@ return {
           vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
           vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
         end,
+      })
+
+      local lspconfig = require('lspconfig')
+      lspconfig.lua_ls.setup{}
+
+      local cmp = require('cmp')
+
+      cmp.setup({
+        sources = {
+          { name = 'nvim_lsp' },
+        },
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({}),
       })
     end,
   }
