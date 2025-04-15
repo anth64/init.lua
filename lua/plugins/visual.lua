7f1a3b82daf846d6bc350ab2304469474371705c
anth64 <anth64@protonmail.com>
2025-04-15 19:29:43 +0200

    add colorschemes, default to moonfly

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -4,8 +4,17 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		lazy = false,
 		config = function()
-		require("lualine").setup()
+			require("lualine").setup()
 		end,
-  	}
+	},
+	{
+		"bluz71/vim-moonfly-colors",
+		lazy = false,
+		config = function()
+			vim.cmd.colorscheme("moonfly")
+		end,
+	},
+	{ "EdenEast/nightfox.nvim", lazy = false },
+	{ "oxfist/night-owl.nvim", lazy = false },
 }
 
