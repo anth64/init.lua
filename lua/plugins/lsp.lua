5e6716e2637bcca21028f0fc0d84278432af43e1
anth64 <anth64@protonmail.com>
2025-05-03 09:54:12 +0200

    added cmp-buffer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 return {
 	{ "neovim/nvim-lspconfig" },
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{ "hrsh7th/cmp-buffer"},
 	{ "hrsh7th/nvim-cmp" },
 	{ "hrsh7th/cmp-nvim-lsp" },
 	{ "mfussenegger/nvim-jdtls" } -- jdtls bindings for neovim
