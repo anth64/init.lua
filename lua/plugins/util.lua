2a789eda7a4fc9cae18aa975007eaf1db48dc464
anth64 <anth64@protonmail.com>
2025-05-28 18:45:18 +0200

    add telescope fzf for native fuzzy find, very fast...

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -13,7 +13,10 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+    },
     config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
